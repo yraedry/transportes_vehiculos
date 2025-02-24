a865b9f66a84de02173885f80ff66cf9c2701192
Mayden <adrianns93@gmail.com>
2025-02-24 13:35:26 +0100

    Update SinMotor.java

--- src/transporte/SinMotor.java
+++ src/transporte/SinMotor.java
@@ -3,7 +3,7 @@ package src.transporte;
 /**
  * interfaz que implementa los metodos necesarios para el uso de la clase.
  */
-public interface SinMotor {
+interface SinMotor {
     /**
      * Metodo necesario para el uso de la interfaz.
      */
