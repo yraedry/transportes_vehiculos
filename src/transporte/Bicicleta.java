c0554c7ccd28408942548c8809ec8997591b2b93
Javier Sánchez <javifullstackdev@gmail.com>
2025-02-28 15:42:19 +0100

    Añadidas clases Barco y Bicicleta

--- src/transporte/Bicicleta.java
+++ src/transporte/Bicicleta.java
@@ -5,18 +5,26 @@ package src.transporte;
 public final class Bicicleta extends Vehiculo implements SinMotor{
 
     /**
-     * TODO
-     * Metodo usarFuerzaHumana, que imprime "Usando fuerza humana para mover la bicicleta."
+     * Método usarFuerzaHumana, que imprime "Usando fuerza humana para mover la bicicleta."
      * el metodo usarFuerzaHumana es una implementación de la interfaz SinMotor. por lo que
-     * hay que sobreescribir el metodo de la interfaz SinMotor.
+     * hay que sobreescribir el método de la interfaz SinMotor.
      */
+
+    @Override
+    public void usarFuerzaHumana() {
+        System.out.println("Usando fuerza humana para mover la bicileta.");
+    }
 
 
     /**
-     * TODO
-     * Metodo describir, que imprime "Soy una bicicleta con una velocidad máxima de 30 km/h."
+     * Método describir, que imprime "Soy una bicicleta con una velocidad máxima de 30 km/h."
      * el metodo describir es una implementación de la clase abstracta Vehiculo. por lo que
-     * hay que sobreescribir el metodo describir de la clase abstracta.
+     * hay que sobreescribir el método describir de la clase abstracta.
      */
+
+    @Override
+    public void describir() {
+        System.out.println("Soy una bicicleta con una velocidad máxima de 30 km/h.");
+    }
 
 }
